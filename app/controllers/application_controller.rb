edee2d1c35dd8ba7b6685dc9dbe208a6bfd3f703
Tommy <spiel.hoelle@live.de>
2018-11-27 17:46:42 +0100

    update hidden tags display + typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :authenticate
   def index
     if !primary_domain
       @categories = Category.joins(:photos).where.not(:hidden => false).group("categories.id")
-      @tags = Photo.joins(:categories).where( categories: { hidden: true }).collect{|p| p.tags}.flatten.uniq
+      @tags = Photo.joins(:categories).where( categories: { hidden: false }).collect{|p| p.tags}.flatten.uniq
     else 
       @categories = Category.joins(:photos).where(:hidden => true).group("categories.id")
-      @tags = Photo.joins(:categories).where( categories: { hidden: false }).collect{|p| p.tags}.flatten.uniq
+      @tags = Photo.joins(:categories).where( categories: { hidden: true }).collect{|p| p.tags}.flatten.uniq
     end
 
     @page_title = "Luciana Damiao"
   end
   def primary_domain
-    request.original_url.include?('localhost:3000') || request.original_url.include?('lucianadamioa')
+    request.original_url.include?('localhost:3000') || request.original_url.include?('lucianadamiao')
   end
 end
