eeeebc55a652f06b735224e4524e36580abe2fc9
spielhoelle <spiel.hoelle@live.de>
2019-03-22 18:33:49 +0100

    photocontroller switch betw. primary domain, round image

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,9 +12,9 @@ class PhotosController < ApplicationController
       @photos = @photos.present? ? @photos.merge(photos) : photos
     end
     if !entry_params[:tag] && !entry_params[:cat]
-      @photos = Photo.where.not(title: "avatar").where("parent_id IS NULL").order(order: :asc)
+      @photos = Photo.joins(:categories).where( categories: { hidden: [nil, "0"] }).where("parent_id IS NULL").order(order: :asc)
       if !primary_domain
-        @photos = Photo.joins(:categories).where( categories: { hidden: true }).where.not(:title => "avatar").where("parent_id IS NULL").order(order: :asc)
+        @photos = Photo.joins(:categories).where( categories: { hidden: 1 }).where.not(:title => "avatar").where("parent_id IS NULL").order(order: :asc)
       end
     end
   end
